babd7b2a35ffe4869c5f69035071c6e69ee79347
Zach Kost-Smith <zachkostsmith@gmail.com>
2012-05-21 06:35:13 -0600

    Added function when connecting streams.
    This function, if given, will be called on every character in the stream.  This
    allows you to do things like the "tee" program does in the shell.

--- cl-plumbing.lisp
+++ cl-plumbing.lisp
@@ -1,24 +1,24 @@
 
 (in-package :cl-plumbing)
 
-(defun dump-streams (input output)
+(defun dump-stream (input output &optional fn)
   (with-open-stream (echo (make-echo-stream input output))
     (iter (for c = (read-char echo nil nil))
+      (when fn (funcall fn c))
       (while c))))
 
 (defvar *warn-on-foreground-connect-stream* t)
 
-(defun connect-streams (input output &optional (background t))
+(defun connect-streams (input output &key (background t) fn)
   "This reads from input and writes output until the end of input is found."
   #-bordeaux-threads
   (progn
     (when (and background *warn-on-foreground-connect-stream*)
       (warn "Unable to run in background without multithreading support"))
-    (dump-streams input output))
+    (dump-stream input output fn))
   #+bordeaux-threads
   (if background
       (bt:make-thread
        (lambda ()
-         (dump-streams input output)))
-      (dump-streams input output)))
-
+         (dump-stream input output fn)))
+      (dump-stream input output fn)))
