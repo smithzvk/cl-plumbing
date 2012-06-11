1a4ddd89f654e4781bd7e0c57dd082b2ef0f5919
Zach Kost-Smith <zachkostsmith@gmail.com>
2012-06-11 14:53:44 -0500

    Bug fix: Protect against buffer underflow eof.
    EOF were being reported whenever the output buffer was emptied.  This is now
    fixed.
    
    Bug fix: proper blocking on read-char and read-line

--- gray-streams-pipe.lisp
+++ gray-streams-pipe.lisp
@@ -24,18 +24,72 @@
              (make-string-input-stream string))))))
 
 (defmethod trivial-gray-streams:stream-read-char ((p pipe))
-  (bt:with-lock-held ((lock-of p))
-    (flush-in-to-out p)
-    (read-char (input-of p) nil :eof)))
+  (iter
+    (bt:with-lock-held ((lock-of p))
+      (let ((eof (not (open-stream-p (output-of p)))))
+        (flush-in-to-out p)
+        (let ((result (read-char (input-of p) nil :eof)))
+          (cond ((not (equal :eof result)) (return result))
+                ((and eof (equal :eof result)) (return :eof))
+                (t nil)))))
+    ;; Is there a way to remove this polling delay?  Perhaps it isn't a big
+    ;; deal.
+    (sleep .01)))
 
 (defmethod trivial-gray-streams:stream-unread-char ((p pipe) character)
   (bt:with-lock-held ((lock-of p))
     (unread-char character (input-of p))))
 
+(defparameter *block-size* 1024)
+
 (defmethod trivial-gray-streams:stream-read-line ((p pipe))
-  (bt:with-lock-held ((lock-of p))
-    (flush-in-to-out p)
-    (read-line (input-of p) nil :eof)))
+  (let ((consumed nil))
+    (unwind-protect
+         (iter
+           (bt:with-lock-held ((lock-of p))
+             (flush-in-to-out p)
+             (let* ((eof (not (open-stream-p (output-of p))))
+                    (seq (make-array (list *block-size*)))
+                    (n-read (read-sequence seq (input-of p)))
+                    (newline-marker (iter (for char in-sequence seq with-index i)
+                                      (while (< i n-read))
+                                      (finding i such-that (eql char #\Newline)))))
+               (cond ((and newline-marker (< newline-marker n-read))
+                      (setf (input-of p) (make-concatenated-stream
+                                          (make-string-input-stream
+                                           (coerce (subseq seq (+ newline-marker 1) n-read)
+                                                   'string))
+                                          (input-of p)))
+                      (let ((c consumed))
+                        (setf consumed nil)
+                        (return (coerce (apply #'concatenate
+                                               'string
+                                               (reverse
+                                                (cons
+                                                 (subseq seq 0 newline-marker)
+                                                 c)))
+                                        'string))))
+                     (eof (let ((c consumed))
+                            (setf consumed nil)
+                            (return
+                              (values (coerce (apply #'concatenate
+                                                     'string
+                                                     (reverse
+                                                      (cons
+                                                       (subseq seq 0 n-read)
+                                                       c)))
+                                              'string) t))))
+                     (t (push (subseq seq 0 n-read) consumed)))))
+           ;; Block until there is more to read.
+           (unread-char (read-char p) p))
+      (setf (input-of p)
+            (apply
+             'make-concatenated-stream
+             (reverse
+              (cons (input-of p)
+                    (mapcar
+                     (lambda (x) (make-string-input-stream (coerce x 'string)))
+                     consumed))))))))
 
 (defmethod trivial-gray-streams:stream-read-sequence
     ((p pipe) seq start end &key &allow-other-keys)
